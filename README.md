0ff0733aa0713589dc7fe6f7a9aef51b64c56513
raunak2007 <raunak2007@users.noreply.github.com>
2026-06-08 20:05:08 +0000

    deploy: 9c8ea48c25c595746b10b1f7113ba3176bb8592c

---
+++ README.md
@@ -0,0 +1,2 @@
+# **Overview**
+This repository contains my reflection of my showcase event Night at the Museum (N@TM) in February of 2024. In addition, I have uploaded many other files to this repository, such as my Jupyter Notebook demonstration of my project using artificial intelligence for autism diagnosis, Development Of Personalized Machine Learning and Data Augmentation Strategies To Diagnose Autism, and my personal resume highlighting the various programming projects that I have done in the past that focus on Full-Stack Development and Machine Learning.
---
+++ js/index.js
@@ -0,0 +1,35 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
+
+console.log("woah how did you get here");
+console.log("nice job human");
